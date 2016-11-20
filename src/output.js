bfe6124209d0c455f9fdf9f1b9b2c7d234a53907
BobMcFry <cityfisch@yahoo.de>
2016-11-20 15:57:27 -0700

    Correct output of tree such that it will work for all input

--- src/output.js
+++ src/output.js
@@ -64,11 +64,8 @@ HenAlgorithmOut.prototype.notify = function() {
 
 HenAlgorithmOut.prototype.getStringRepresentation = function() {
 
-	var tree = this.henTreeOut.getTree();
 	var list = this.listOut.getList();
 	var listString = "";
-	var string = "";
-
 
 	// LIST of notices
 	listString += "[-]"
@@ -80,22 +77,36 @@ HenAlgorithmOut.prototype.getStringRepresentation = function() {
 	}
 	listString += "[+]"
 
+	var treeString = this.getTreeStringRepresentation(listString.length);
+
+	treeString += "\n\n" + listString;
+
+	return treeString;
+
+}
+
+HenAlgorithmOut.prototype.getTreeStringRepresentation = function(width) {
+
 
 	// TREE
+	var string = "";
+	var tree = this.henTreeOut.getTree();
 	var height = tree.length;
 	var noOfLeafs = tree[height-1].length;
-	var treeWidth = listString.length;
+	var lvlStrings = [];
 	var space;
+	var noNodes;
 
 	for (var lvl = 0; lvl < height; lvl++) {
-
-		space = (treeWidth-(tree[lvl].length * this.NODESPACE)) / (tree[lvl].length);
+		
+		noNodes = tree[lvl].length;
+		space = (width-(noNodes * this.NODESPACE)) / noNodes;
 
 		// add half before first
-		string += this.getSpaceString((1/2) * space);
+		string += this.getSpaceString(0.5 * space);
 
 		// print nodes with complete space between
-		for (var i = 0; i < tree[lvl].length; i++) {
+		for (var i = 0; i < noNodes; i++) {
 			// TODO: make this sprintf dependent on NODESPACE!
 			if (tree[lvl][i].time < this.henAlgo.eventList.MIN_VALUE*0.5) {
 				string += sprintf("%"+this.NODESPACE+"s", "[-]");
@@ -105,34 +116,110 @@ HenAlgorithmOut.prototype.getStringRepresentation = function() {
 				string += sprintf("%"+this.NODESPACE+"."+this.FLOATSPACE+"f", tree[lvl][i].time);
 			}
 			
-			if (i < tree[lvl].length-1) {
+			if (i < noNodes - 1) {
 				string += this.getSpaceString(space);
 			}
 		}
 		
 		// add half after last
-		string += this.getSpaceString((1/2) * space);
+		string += this.getSpaceString(0.5 * space);
 
-		// add newline
-		string += "\n";
+		lvlStrings.push(string);
+		string = "";
+	}
 
-	}	
+	
+	var connStrings = [];
+	for (var i = 0; i < tree.length-1; i++) {
 
-	// between list and tree we need some space my dear.
-	string += "\n";
+		// first create a space string and then replace everything
+		connString = this.getSpaceString(width);
+		currentString = lvlStrings[i];
+		nextString = lvlStrings[i+1];
+		var leftSon = 0;
+		var rightSon = 0;
+		var father = 0;
 
-	string += listString;
+		// iterate over string and collect indices of the parts
+		var splittedCurrentString = currentString.trim().match(/\S+/g) || [];
 
-	return string;
+		// iterate over string and collect indices of the parts
+		var splittedNextString = nextString.trim().match(/\S+/g) || [];
 
+		for (var j = 0; j < tree[i].length; j++) {
+			
+			var father  = splittedCurrentString[j];
+			var leftSon = splittedNextString[2*j+0];
+			var rightSon = splittedNextString[2*j+1];
+
+			// get left sons index
+			idxLeftSon = nextString.indexOf(leftSon);
+			nextString = this.replaceAt(nextString, 0, this.getSpaceString(idxLeftSon+this.NODESPACE));
+			// get currentNodes index
+			idxCurrentNode = currentString.indexOf(father);
+			currentString = this.replaceAt(currentString, 0, this.getSpaceString(idxCurrentNode+this.NODESPACE));
+			// getright sons index
+			idxRightSon = nextString.indexOf(rightSon);
+			nextString = this.replaceAt(nextString, 0, this.getSpaceString(idxRightSon+this.NODESPACE));
+
+			connString = this.replaceAt(connString, idxLeftSon, ".");
+			connString = this.replaceAt(connString, idxLeftSon+1, this.getSymbolString(idxCurrentNode - idxLeftSon, "-"));
+			connString = this.replaceAt(connString, idxCurrentNode, "´`");
+			connString = this.replaceAt(connString, idxCurrentNode+2, this.getSymbolString(idxRightSon - idxCurrentNode, "-"));
+			connString = this.replaceAt(connString, idxRightSon+1, ".");
+
+		}
+
+		connStrings.push(connString);
+
+	}
+
+	var joinedString = ""
+	for(var i = 0; i < lvlStrings.length; i++) {
+		joinedString += lvlStrings[i];
+		joinedString += "\n";
+		if (i < lvlStrings.length-1){
+
+			joinedString += connStrings[i];	
+			joinedString += "\n";
+		}
+	}
+
+	return joinedString;
+
+};
+
+HenAlgorithmOut.prototype.replaceAt = function(string, index, characters) {
+    return string.substring(0, index) + characters + string.substring(index+characters.length);
 }
 
-
 HenAlgorithmOut.prototype.getBottomSpaceString = function() {
-	return Array(this.BOTTOMSPACE+1).join(" ");
+	return this.getSymbolString(this.BOTTOMSPACE, " ");
 };
+
 
 HenAlgorithmOut.prototype.getSpaceString = function(no) {
-	return Array(Math.floor(no+1)).join(" ");
+	return this.getSymbolString(no, " ");
 };
 
+
+HenAlgorithmOut.prototype.getSymbolString = function(length, symbol) {
+	if (length+1 < 1) {
+		length = 0;
+	}
+	return Array(Math.floor(length+1)).join(symbol);
+};
+
+
+function logg (obj){
+// console.log(obj);
+// debugCon(obj);
+}
+
+var debugCnt = 1;
+function debugCon (title, obj){
+// $('#debugCon').text($('#debugCon').text() + obj);
+// $('#debugCon').text($('#debugCon').text() + "<b>");
+$('#debugCon').append("<p>"+ (debugCnt++) + ": "  +title +  "</p>");
+$('#debugCon').append("<pre>"+obj+ "</pre>");
+}
